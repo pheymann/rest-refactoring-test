e5017eae9976c96188d4cb45dede958079dfc4ad
paul-heymann <paul.heymann@xing.com>
2017-01-21 16:10:35 +0100

    Moved ParamsBuilder function into case class.

--- core/src/main/scala/com/github/pheymann/rrt/ParamsBuilder.scala
+++ core/src/main/scala/com/github/pheymann/rrt/ParamsBuilder.scala
@@ -4,7 +4,30 @@ import com.github.pheymann.rrt.util.RandomUtil
 
 import scala.collection.mutable
 
-final case class ParamsBuilder(params: mutable.Builder[(String, String), Map[String, String]])
+final case class ParamsBuilder(params: mutable.Builder[(String, String), Map[String, String]]) {
+
+  private def add(key: String, value: String): ParamsBuilder = {
+    this.params += key -> value
+    this
+  }
+
+  private def addOpt(key: String, value: String)(implicit rand: RandomUtil): ParamsBuilder = {
+    if (rand.rand.nextBoolean())
+      add(key, value)
+
+    this
+  }
+
+  def add[A](key: String, value: A): ParamsBuilder = add(key, value.toString)
+  def addOpt[A](key: String, value: A)(implicit rand: RandomUtil): ParamsBuilder = addOpt(key, value.toString)
+
+  def add[A](key: String, value: () => A): ParamsBuilder = add(key, value().toString)
+  def addOpt[A](key: String, value: () => A)(implicit rand: RandomUtil): ParamsBuilder = addOpt(key, value().toString)
+
+  def add[A](key: String, value: Seq[A]): ParamsBuilder = add(key, value.mkString(","))
+  def addOpt[A](key: String, value: Seq[A])(implicit rand: RandomUtil): ParamsBuilder = addOpt(key, value.mkString(","))
+
+}
 
 trait ParamsBuilderSyntax {
 
@@ -12,31 +35,6 @@ trait ParamsBuilderSyntax {
 
     def apply(): ParamsBuilder = ParamsBuilder(Map.newBuilder[String, String])
     def apply(params: mutable.Builder[(String, String), Map[String, String]]): ParamsBuilder = ParamsBuilder(params)
-
-  }
-
-  implicit class ParamsBuilderExtension(builder: ParamsBuilder) {
-
-    private def add(key: String, value: String): ParamsBuilder = {
-      builder.params += key -> value
-      builder
-    }
-
-    private def addOpt(key: String, value: String)(implicit rand: RandomUtil): ParamsBuilder = {
-      if (rand.rand.nextBoolean())
-        add(key, value)
-
-      builder
-    }
-
-    def add[A](key: String, value: A): ParamsBuilder = add(key, value.toString)
-    def addOpt[A](key: String, value: A)(implicit rand: RandomUtil): ParamsBuilder = addOpt(key, value.toString)
-
-    def add[A](key: String, value: () => A): ParamsBuilder = add(key, value().toString)
-    def addOpt[A](key: String, value: () => A)(implicit rand: RandomUtil): ParamsBuilder = addOpt(key, value().toString)
-
-    def add[A](key: String, value: Seq[A]): ParamsBuilder = add(key, value.mkString(","))
-    def addOpt[A](key: String, value: Seq[A])(implicit rand: RandomUtil): ParamsBuilder = addOpt(key, value.mkString(","))
 
   }
 
