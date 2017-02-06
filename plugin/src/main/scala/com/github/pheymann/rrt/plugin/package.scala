1a631b06b67603ab2c59f6f985883cfa3b5cfb38
paul-heymann <paul.heymann@xing.com>
2017-02-06 15:30:35 +0100

    Set version to 2.2.1.

--- plugin/src/main/scala/com/github/pheymann/rrt/plugin/package.scala
+++ plugin/src/main/scala/com/github/pheymann/rrt/plugin/package.scala
@@ -12,7 +12,7 @@ package object plugin {
     resourceDirectory in RestRefactoringTest := baseDirectory.value / "test-conf"
   )
 
-  val rrtVersion = "2.2.0"
+  val rrtVersion = "2.2.1"
 
   val rrtCore = "com.github.pheymann" %% "rrt-core" % rrtVersion
   val rrtPlay = "com.github.pheymann" %% "rrt-play" % rrtVersion
